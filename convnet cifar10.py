5008c1843471785f601294c0e1d27fe36c3aa7cc
Aden King <adenking@live.com>
2018-10-14 19:24:28 +1000

    Add files via upload

---
+++ convnet cifar10.py
@@ -0,0 +1,81 @@
+# run tensorboard to see results
+
+# import statements
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+import time
+
+CATEGORIES = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
+IMG_SIZE = 32
+num_classes = 10
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+print('x_train shape:', x_train.shape)
+print('y_train shape:', y_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+
+plt.imshow(x_train[1])
+plt.show()
+print(y_train[1])
+
+for sample in y_train[:10]:
+    print(sample[0])
+
+X = x_train
+y = y_train
+
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 3)
+print(X.shape)
+print(y.shape)
+X = X / 255
+
+epochs = 20
+
+dense_layers = [0]
+layer_sizes = [32]
+conv_layers = [3]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            print(NAME)
+
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            for l in range(conv_layer):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            model.add(Flatten())
+
+            for _ in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+
+            model.add(Dense(10))
+            model.add(Activation('sigmoid'))
+
+            tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+            model.compile(loss='sparse_categorical_crossentropy',
+                          optimizer='adam',
+                          metrics=['accuracy'],
+                          )
+
+            model.fit(X, y,
+                      batch_size=32,
+                      epochs=20,
+                      validation_split=0.2,
+                      callbacks=[tensorboard])
