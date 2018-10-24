75bdd4150fc65dcd5f945a9728ddee3eefbf6c40
Aden King <adenking@live.com>
2018-10-24 10:56:21 +1000

    remove variable checks move towards building the best accuracy model

--- convnet cifar10.py
+++ convnet cifar10.py
@@ -37,49 +37,45 @@ X = X / 255
 
 epochs = 20
 
-dense_layers = [0]
-layer_sizes = [32, 64, 128]
-conv_layers = [0, 1, 2]
 
-for dense_layer in dense_layers:
-    for layer_size in layer_sizes:
-        for conv_layer in conv_layers:
-            NAME = "{}-conv-{}-nodes-{}-dense-dropout-{}".format(conv_layer, layer_size, dense_layer, int(time.time()))
-            print(NAME)
+layer_size = 128
+conv_layers = 2
 
-            model = Sequential()
 
-            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
-            model.add(Activation('relu'))
-            model.add(MaxPooling2D(pool_size=(2, 2)))
+NAME = "{}-nodes-dropout-{}".format(layer_size, int(time.time()))
+print(NAME)
 
-            for l in range(conv_layer):
-                model.add(Conv2D(layer_size, (3, 3)))
-                model.add(Activation('relu'))
-                model.add(MaxPooling2D(pool_size=(2, 2)))
+model = Sequential()
 
-            model.add(Dropout(0.2))
+model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-            model.add(Flatten())
+model.add(Conv2D(layer_size, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-            for _ in range(dense_layer):
-                model.add(Dense(layer_size))
-                model.add(Activation('relu'))
+model.add(Conv2D(layer_size, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
 
-            model.add(Dense(10))
-            model.add(Activation('sigmoid'))
+model.add(Flatten())
 
-            tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+model.add(Dense(10))
+model.add(Activation('sigmoid'))
 
-            model.compile(loss='sparse_categorical_crossentropy',
-                          optimizer='adam',
-                          metrics=['accuracy'],
-                          )
+tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
 
-            model.fit(X, y,
-                      batch_size=32,
-                      epochs=epochs,
-                      validation_split=0.2,
-                      callbacks=[tensorboard])
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'],
+              )
 
-            model.save(f'./models/{conv_layer}-conv-{layer_size}-nodes-{dense_layer}-dense-dropout-{int(time.time())}.h5')
+model.fit(X, y,
+          batch_size=32,
+          epochs=epochs,
+          validation_split=0.2,
+          callbacks=[tensorboard])
+
+model.save(f'./models/{conv_layer}-conv-{layer_size}-nodes-{dense_layer}-dense-dropout-{int(time.time())}.h5')
